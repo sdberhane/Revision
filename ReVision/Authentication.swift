d569ab27babe70ba17358f9f082be033bca6c4d9
Jacob Marsh <s013687@lm-20s013687.lmsd.org>
2019-01-03 10:05:28 -0500

    Added Authentication Class

---
+++ ReVision/Authentication.swift
@@ -0,0 +1,9 @@
+//
+//  Authentication.swift
+//  ReVision
+//
+//  Created by Jacob Marsh (student LM) on 1/3/19.
+//  Copyright © 2019 Eugenia Feng (student LM). All rights reserved.
+//
+
+import Foundation
