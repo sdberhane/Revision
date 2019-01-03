f01f5f9c5c2328dffe165085ce32312959279e0d
Jacob Marsh <s013687@lm-20s013687.lmsd.org>
2019-01-03 09:53:01 -0500

    Jacob is here

--- ReVision/ViewController.swift
+++ ReVision/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Hello world!")
         print("goodbye world!")
+        print("jacob in the house")
     }
 
 
