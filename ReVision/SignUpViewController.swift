e5b80d4e3374a308b4bb7262cdfa30f592c63a8e
Eugenia Feng <eugeniafeng@gmail.com>
2019-01-10 09:31:30 -0500

    Sign up view controller functionality added

--- ReVision/SignUpViewController.swift
+++ ReVision/SignUpViewController.swift
@@ -10,21 +10,62 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
-    @IBAction func signUpButton(_ sender: UIButton) {
-    }
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBAction func nextButton(_ sender: UIButton) {
     }
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var schoolNameTextField: UITextField!
+    @IBAction func signUpButton(_ sender: UIButton) {
+        guard let email = emailTextField.text else {return}
+        guard let password = passwordTextField.text else {return}
+        guard let username = usernameTextField.text else {return}
+        guard let school = schoolNameTextField.text else {return}
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            if user != nil, error == nil{
+                print("user created")
+                self.dismiss(animated: true, completion: nil)
+            }
+            else{
+                print(error.debugDescription)
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        usernameTextField.delegate = self
+        schoolNameTextField.delegate = self
+        
+        emailTextField.becomeFirstResponder()
 
         // Do any additional setup after loading the view.
     }
     
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if emailTextField.isFirstResponder {
+            passwordTextField.becomeFirstResponder()
+        }
+        else if passwordTextField.isFirstResponder {
+            usernameTextField.becomeFirstResponder()
+        }
+        else if usernameTextField.isFirstResponder {
+            schoolNameTextField.becomeFirstResponder()
+        }
+        else if schoolNameTextField.isFirstResponder {
+            passwordTextField.becomeFirstResponder()
+        }
+        else {
+            passwordTextField.resignFirstResponder()
+            signUpButton.isEnabled = true
+        }
+        return true
+    }
     /*
     // MARK: - Navigation
 
