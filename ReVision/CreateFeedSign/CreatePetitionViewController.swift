5ee95488801c30c6a6d0d556659dccf0c8933a6c
Sihan Wu <s020059@students.lmsd.org>
2019-02-01 10:15:26 -0500

    add create petition class

---
+++ ReVision/CreateFeedSign/CreatePetitionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CreatePetitionViewController.swift
+//  ReVision
+//
+//  Created by Sihan Wu (student LM) on 2/1/19.
+//  Copyright © 2019 Eugenia Feng (student LM). All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+import FirebaseAuth
+import FirebaseStorage
+
+class CreatePetitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
