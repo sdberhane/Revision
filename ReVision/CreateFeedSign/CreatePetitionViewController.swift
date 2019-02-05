b8a6da06b4cd2c061ec5d996d8d232f84a2df3aa
Sihan Wu <s020059@students.lmsd.org>
2019-02-05 07:26:04 -0500

    changes

--- ReVision/CreateFeedSign/CreatePetitionViewController.swift
+++ ReVision/CreateFeedSign/CreatePetitionViewController.swift
@@ -13,14 +13,16 @@ import FirebaseStorage
 
 class CreatePetitionViewController: UIViewController {
     var ref = Database.database().reference().child("Active Petitions").child("uid")
+    var petition:Petition?
+    let userID = Auth.auth().currentUser?.uid
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var subtitleTextView: UITextView!
     @IBOutlet weak var goalTextField: UITextField!
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBAction func createPetitionButton(_ sender: UIButton) {
-        Petition(title: titleTextField.text ?? " ", description: descriptionTextView.text, creator: userID ?? " ", goalSignatures: Int(goalTextField.text ?? "0") ?? 0, signatures: [String](), anonymous: false)
+        petition = Petition(title: titleTextField.text ?? " ", description: descriptionTextView.text, creator: userID ?? " ", goalSignatures: Int(goalTextField.text ?? "0") ?? 0, signatures: [String](), anonymous: false)
+        
     }
-    let userID = Auth.auth().currentUser?.uid
     
     
     override func viewDidLoad() {
