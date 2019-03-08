2c2c889295d71f757b08844733f6fcd48a51170c
Eugenia Feng <eugeniafeng@gmail.com>
2019-03-08 09:25:45 -0500

    log out button

--- ReVision/Settings Controllers/SettingsViewController.swift
+++ ReVision/Settings Controllers/SettingsViewController.swift
@@ -7,22 +7,19 @@
 //
 
 import UIKit
+import FirebaseAuth
 
-class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SettingsViewController: UIViewController {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "createdPetitions", for: indexPath) as! PetitionTableViewCell
-        if indexPath.row == 1 {
-            
+    @IBAction func logOutButton(_ sender: UIButton) {
+        do {
+            try Auth.auth().signOut()
+            self.dismiss(animated: true, completion: nil)
+        } catch let err {
+            print(err)
         }
-        return cell
     }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
