dd7ebd6cbcc6b1ea1639ff80e5b09ac60c0c3743
Eugenia Feng <eugeniafeng@gmail.com>
2019-03-11 10:15:56 -0400

    second feed

---
+++ ReVision/CreateFeedSign/SecondFeedViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondFeedViewController.swift
+//  ReVision
+//
+//  Created by Eugenia Feng (student LM) on 3/11/19.
+//  Copyright © 2019 Eugenia Feng (student LM). All rights reserved.
+//
+
+import UIKit
+
+class SecondFeedViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
