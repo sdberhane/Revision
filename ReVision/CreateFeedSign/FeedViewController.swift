9dcafe793e1ba91c65ac7ae0d8ddfc18dad13f20
Eugenia Feng <eugeniafeng@gmail.com>
2019-03-22 11:40:00 -0400

    second feed done

--- ReVision/CreateFeedSign/FeedViewController.swift
+++ ReVision/CreateFeedSign/FeedViewController.swift
@@ -128,15 +128,24 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
 
          NotificationCenter.default.addObserver(self, selector: #selector(showSeniorTag), name: NSNotification.Name("ShowSenior"), object: nil)
 
-//        NotificationCenter.default.addObserver(self, selector: #selector(showParentsTag), name: NSNotification.Name("ShowCreatedPetitions"), object: nil)
-//
-//        NotificationCenter.default.addObserver(self, selector: #selector(showTeachersTag), name: NSNotification.Name("ShowTeachersTag"), object: nil)
-//
-//        NotificationCenter.default.addObserver(self, selector: #selector(showSportsTag), name: NSNotification.Name("ShowSportsTag"), object: nil)
-//
-//        NotificationCenter.default.addObserver(self, selector: #selector(showClubsTag), name: NSNotification.Name("ShowClubsTag"), object: nil)
-//
-//        NotificationCenter.default.addObserver(self, selector: #selector(showAcademicsTag), name: NSNotification.Name("ShowAcademics"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(showParentsTag), name: NSNotification.Name("ShowParents"), object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(showTeachersTag), name: NSNotification.Name("ShowTeachers"), object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(showSportsTag), name: NSNotification.Name("ShowSports"), object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(showClubsTag), name: NSNotification.Name("ShowClubs"), object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(showAcademicsTag), name: NSNotification.Name("ShowAcademics"), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(showGraduationTag), name: NSNotification.Name("ShowGraduation"), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(showFacilitiesTag), name: NSNotification.Name("ShowFacilities"), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(showScheduleTag), name: NSNotification.Name("ShowSchedule"), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(showOtherTag), name: NSNotification.Name("ShowOther"), object: nil)
+
     }
 
         
@@ -222,7 +231,42 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         performSegue(withIdentifier: "showSelectedPetitions", sender: 6)
     }
 
+    @objc func showParentsTag() {
+        performSegue(withIdentifier: "showSelectedPetitions", sender: 7)
+    }
+    
+    @objc func showTeachersTag() {
+        performSegue(withIdentifier: "showSelectedPetitions", sender: 8)
+    }
+    
+    @objc func showSportsTag() {
+        performSegue(withIdentifier: "showSelectedPetitions", sender: 9)
+    }
+    
+    @objc func showClubsTag() {
+        performSegue(withIdentifier: "showSelectedPetitions", sender: 10)
+    }
 
+    @objc func showAcademicsTag() {
+        performSegue(withIdentifier: "showSelectedPetitions", sender: 11)
+    }
+    
+    @objc func showGraduationTag() {
+        performSegue(withIdentifier: "showSelectedPetitions", sender: 12)
+    }
+    
+    @objc func showFacilitiesTag() {
+        performSegue(withIdentifier: "showSelectedPetitions", sender: 13)
+    }
+    
+    @objc func showScheduleTag() {
+        performSegue(withIdentifier: "showSelectedPetitions", sender: 14)
+    }
+    
+    @objc func showOtherTag() {
+        performSegue(withIdentifier: "showSelectedPetitions", sender: 15)
+    }
+    
 //    @objc func showHomescreen() {
 //        performSegue(withIdentifier: "ShowSignIn", sender: nil)
 //    }
