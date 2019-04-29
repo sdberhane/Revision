10d3b69d28707ad7504ef4a4d6f29bd0dd207be8
Eugenia Feng <eugeniafeng@gmail.com>
2019-04-28 23:19:51 -0400

    petitiontableviewcell comments

--- ReVision/CreateFeedSign/PetitionTableViewCell.swift
+++ ReVision/CreateFeedSign/PetitionTableViewCell.swift
@@ -12,6 +12,7 @@ import FirebaseDatabase
 import FirebaseStorage
 
 class PetitionTableViewCell: UITableViewCell {
+    // outlets and variables
     @IBOutlet weak var petitionSubtitle: UILabel!
     @IBOutlet weak var petitionTitle: UILabel!
     @IBOutlet weak var petitionImage: UIImageView!
@@ -23,7 +24,9 @@ class PetitionTableViewCell: UITableViewCell {
     var id: String?
     var active: Bool = false
 
+    // saving petitiosn when users click the bookmarks
     @IBAction func save(_ sender: Any) {
+        // saving the petition under the user's branch in Firebase
         let userid = Auth.auth().currentUser?.uid
         guard let uid = userid else {return}
         let time = Date().description
@@ -33,7 +36,7 @@ class PetitionTableViewCell: UITableViewCell {
         saveForLater.image = #imageLiteral(resourceName: "saved4later.png")
     }
 
-    
+    // default methods
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
